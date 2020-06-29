1560d358d7d9fbd28247f768c74e0686aa94c576
pere237 <alexis.perez@arvato.com>
2020-06-30 00:52:43 +0200

    first commit

--- src/main/java/testing/FirefoxDriver/App.java
+++ src/main/java/testing/FirefoxDriver/App.java
@@ -1,13 +1,10 @@
 package testing.FirefoxDriver;
 
 /**
- * Hello world!
- *
+ * Hello world! test 1
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
